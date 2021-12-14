729ca18e5438d8f2197dcab4f98120f0fbe5da3e
indhujaa <indhujaa1998@gmail.com>
2021-12-15 00:42:59 +0530

    changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV['SANDGRID_USERNAME'],
+  :password => ENV['SANDGRID_PASSWORD'], # This is the secret sendgrid API key which was issued during API key creation
+  :domain => 'heroku.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
